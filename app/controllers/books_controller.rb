13a460173e80f4402468ce1f7955a12deef8467a
EC2 Default User <ec2-user@ip-172-31-42-2.ec2.internal>
2020-10-08 04:28:59 +0000

    [update]詳細ページは出来たがindexに入力したデータ反映されていない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,23 +4,28 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+  #string parameters定義
+
   end
 
   def new
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path(book.id)
   end
 
   def edit
   end
-  
+
    private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
+    #params.require(:book).permit(:title, :body)
+
   end
 end
